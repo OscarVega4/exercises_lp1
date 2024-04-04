70b5be3e6f631985cf8ebdf3dc54ecc977f8c113
OscarVega4 <oacevedo@cesde.net>
2024-04-03 20:09:22 -0500

    ejercicios

--- src/main/java/com/cesde/Ejercicios.java
+++ src/main/java/com/cesde/Ejercicios.java
@@ -1,29 +1,34 @@
 package com.cesde;
 
 public class Ejercicios {
-    
+
     public String ejercicio1(int num1, int num2) {
+        // Implementar solución
+        if (num1 > num2) {
+            return "El número mayor es: " + num1;
+        } else if (num2 > num1) {
+            return "El número mayor es: " + num2;
+        } else {
+            return "Los dos números son iguales.";
+        }
+    }
+
+    public String ejercicio2(double calificacion) {
         // Implementar solución
         return "";
     }
-    
-    public String ejercicio2(double calificacion) {
-       // Implementar solución
-        return "";
-    }
 
-    
     public double ejercicio3(double precio) {
         // Implementar solución
         return 0;
     }
-    
+
     public String ejercicio4(int tipoFigura, double ladoCuadrado, double baseTriangulo, double alturaTriangulo,
             double radioCirculo) {
         // Implementar solución
         return "";
     }
-    
+
     public int ejercicio5(int num1, int num2, int num3) {
         // Implementar solución
         return 0;
